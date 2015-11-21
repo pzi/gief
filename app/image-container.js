3bc83cec2d685f41a5780e3944beed9a02e89df4
Patrik Affentranger <patrik.affentranger@gmail.com>
2015-11-21 14:03:03 +0800

    Move all image related functionality out into a class

---
+++ app/image-container.js
@@ -0,0 +1,111 @@
+const Menu = remote.require('menu');
+const MenuItem = remote.require('menu-item');
+
+// Constructor
+function ImageContainer(wrapper) {
+  // always initialize all instance properties
+  this.wrapper = wrapper;
+}
+
+ImageContainer.prototype.addNew = function (image) {
+  // create the container
+  const imageContainer = document.createElement('div');
+  imageContainer.className = 'image-container';
+
+  // create the image link and add it to the container
+  const link = document.createElement('a');
+  link.className = 'image-link';
+  link.href = image.url;
+  link.addEventListener('click', this.select.bind(this));
+  link.addEventListener('dblclick', this.copyImgUrlToClipboard.bind(this));
+  link.addEventListener('contextmenu', this.openImageContextMenu.bind(this));
+  imageContainer.appendChild(link);
+
+  // create the actual img and add it inside the link
+  const img = document.createElement('img');
+  img.src = image.url;
+  link.appendChild(img);
+
+  // create all tags and add it to the container
+  const keywords = image.keywords.split(' ');
+  const tags = document.createElement('p');
+  tags.className = 'tags';
+  for (var i = 0; i < keywords.length; i++) {
+    // only use keywords that are longer than 1 character
+    if ((keyword = keywords[i]).length > 1) {
+      var tag = document.createElement('span');
+      tag.className = 'tag';
+      tag.innerHTML = keyword;
+      tags.appendChild(tag);
+    }
+  };
+  imageContainer.appendChild(tags);
+
+  // append the finished container to target wrapper
+  this.wrapper.appendChild(imageContainer);
+};
+
+ImageContainer.prototype.select = function (target) {
+  var imageContainer = '';
+  const isEvent = target instanceof Event;
+
+  if (isEvent) {
+    const event = target;
+    event.preventDefault();
+    imageContainer = event.currentTarget.parentElement;
+  } else {
+    imageContainer = target;
+  }
+
+  imageContainer ? this.deselectAll() : false;
+
+  if (!imageContainer.classList.contains('is-selected')) {
+    imageContainer.classList.add('is-selected');
+    imageContainer.scrollIntoViewIfNeeded();
+  }
+};
+
+ImageContainer.prototype.deselectAll = function (image) {
+  const imageContainers = this.wrapper.querySelectorAll('.image-container.is-selected');
+  for (var i = imageContainers.length - 1; i >= 0; i--) {
+    imageContainers[i].classList.remove('is-selected');
+  };
+};
+
+ImageContainer.prototype.getImgUrl = function (image) {
+  const isEvent = image instanceof Event;
+  if (isEvent) {
+    return event.target.src;
+  } else {
+    return image.src;
+  }
+};
+
+ImageContainer.prototype.copyImgUrlToClipboard = function (event) {
+  event.preventDefault();
+  clipboard.writeText(event.target.src);
+  currentWindow.hide();
+};
+
+ImageContainer.prototype.openImageContextMenu = function (event) {
+  const self = this;
+  event.preventDefault();
+  this.select(event);
+
+  const menu = new Menu();
+  menu.append(
+    new MenuItem({
+      label: 'Copy URL',
+      accelerator: 'Command+C',
+      click: () => {
+        self.copyImgUrlToClipboard(event);
+      }
+    })
+  );
+
+  setTimeout(() => {
+    menu.popup(currentWindow);
+  }, 10);
+};
+
+module.exports = ImageContainer;
--- app/index.js
+++ app/index.js
@@ -3,12 +3,13 @@ const path = require('path');
 const clipboard = require('clipboard');
 const ipcRenderer = require('ipc-renderer');
 const remote = require('remote');
-const Menu = remote.require('menu');
-const MenuItem = remote.require('menu-item');
 const search = require('./search.js');
+const ImageContainer = require('./image-container.js');
 
 const currentWindow = remote.getCurrentWindow();
-const body = document.querySelector('body');
+
+const imagesWrapper = document.getElementById('images');
+const imageContainer = new ImageContainer(imagesWrapper);
 const searchInput = document.getElementById('search');
 const images = getImages();
 
@@ -18,105 +19,28 @@ function getImages () {
   return dotGifwit.images;
 };
 
-function writeImageUrlToClipBoard (event) {
-  event.preventDefault();
-  const clickedImage = event.target;
-  clipboard.writeText(clickedImage.src);
-  currentWindow.hide();
-}
-
-function openImageContextMenu (event) {
-  event.preventDefault();
-  selectImage(event);
-
-  const menu = new Menu();
-  menu.append(
-    new MenuItem({
-      label: 'Copy URL',
-      accelerator: 'Command+C',
-      click: () => {
-        writeImageUrlToClipBoard(event);
-      }
-    })
-  );
-
-  setTimeout(() => {
-    menu.popup(currentWindow);
-  }, 10);
-}
-
-function selectImage (target) {
-  var imageContainer = '';
-  var isEvent = target instanceof Event;
-
-  if (isEvent) {
-    var event = target;
-    event.preventDefault();
-    imageContainer = event.currentTarget.parentElement;
-  } else {
-    imageContainer = target;
-  }
-
-  imageContainer ? deselectAllImageContainers() : false;
-
-  if (!imageContainer.classList.contains('is-selected')) {
-    imageContainer.classList.add('is-selected');
-    imageContainer.scrollIntoViewIfNeeded();
-  }
-}
-
-function deselectAllImageContainers () {
-  var imageContainers = document.querySelectorAll('.image-container.is-selected');
-  for (var i = imageContainers.length - 1; i >= 0; i--) {
-    imageContainers[i].classList.remove('is-selected');
-  };
-}
-
-for (var i = images.length - 1; i >= 0; i--) {
+for (var i = 0; i < images.length; i++) {
   var image = images[i];
-
-  var imageContainer = document.createElement('div');
-  imageContainer.className = 'image-container';
-
-  var link = document.createElement('a');
-  link.className = 'image-link';
-  link.href = image.url;
-  link.addEventListener('click', selectImage);
-  link.addEventListener('dblclick', writeImageUrlToClipBoard);
-  link.addEventListener('contextmenu', openImageContextMenu);
-
-  var img = document.createElement('img');
-  img.src = image.url;
-  link.appendChild(img);
-
-  var tags = document.createElement('p');
-  tags.className = 'tags';
-  var keywordsArr = image.keywords.split(' ');
-  for (var y = keywordsArr.length - 1; y >= 0; y--) {
-    var tag = document.createElement('span');
-    tag.className = 'tag';
-    tag.innerHTML = keywordsArr[y];
-    if (tag.innerHTML.length > 1) tags.appendChild(tag);
-  };
-
-  imageContainer.appendChild(link);
-  imageContainer.appendChild(tags);
-  body.appendChild(imageContainer);
+  imageContainer.addNew(image);
 };
 
 document.addEventListener('keydown', (event) => {
   const upArrow = 38;
   const downArrow = 40;
-  var currentSelection = document.querySelector('.image-container.is-selected');
+  var currentSelection = imagesWrapper.querySelector('.image-container.is-selected');
 
   if (currentSelection) {
     event.preventDefault();
     if (event.keyCode === upArrow) {
-      if (currentSelection.previousSibling.matches('.image-container'))
-        selectImage(currentSelection.previousSibling);
+      var previousContainer = currentSelection.previousSibling;
+      if (previousContainer && previousContainer.matches('.image-container')) {
+        imageContainer.select(previousContainer);
+      }
     } else if (event.keyCode === downArrow) {
-      if (currentSelection.nextSibling.matches('.image-container'))
-        selectImage(currentSelection.nextSibling);
+      var nextContainer = currentSelection.nextSibling;
+      if (nextContainer && nextContainer.matches('.image-container')) {
+        imageContainer.select(nextContainer);
+      }
     }
   }
 }, true);
@@ -128,11 +52,11 @@ searchInput.addEventListener('input', (event) => {
 });
 
 ipcRenderer.on('window-blur', function () {
-  deselectAllImageContainers();
+  imageContainer.deselectAll();
 });
 
 ipcRenderer.on('GlobalShortcuts', function (event, accelerator) {
   // TODO: Switch to handle different accelerators
-  const selectedImageContainer = document.querySelector('.is-selected');
+  const selectedImageContainer = imagesWrapper.querySelector('.is-selected');
   clipboard.writeText(selectedImageContainer.getElementsByTagName('img')[0].src);
 });
